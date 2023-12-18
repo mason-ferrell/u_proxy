d7202379fac03873866e26bd8f0a9a886f5e01d5
mason-ferrell <59516149+mason-ferrell@users.noreply.github.com>
2023-12-18 11:27:49 -0700

    Add files via upload

---
+++ uproxy/test_local.sh
@@ -0,0 +1,10 @@
+curl --proxy localhost:8888 http://localhost:8080/images/wine3.jpg > 0.jpg &
+curl --proxy localhost:8888 http://localhost:8080/images/wine3.jpg > 1.jpg &
+curl --proxy localhost:8888 http://localhost:8080/images/wine3.jpg > 2.jpg &
+curl --proxy localhost:8888 http://localhost:8080/images/wine3.jpg > 3.jpg &
+curl --proxy localhost:8888 http://localhost:8080/images/wine3.jpg > 4.jpg &
+curl --proxy localhost:8888 http://localhost:8080/images/wine3.jpg > 5.jpg &
+curl --proxy localhost:8888 http://localhost:8080/images/wine3.jpg > 6.jpg &
+curl --proxy localhost:8888 http://localhost:8080/images/wine3.jpg > 7.jpg &
+curl --proxy localhost:8888 http://localhost:8080/images/wine3.jpg > 8.jpg &
+curl --proxy localhost:8888 http://localhost:8080/images/wine3.jpg > 9.jpg &
